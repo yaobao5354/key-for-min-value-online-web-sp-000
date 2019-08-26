1d06f4da82862b136793d68b0c4caa15ba728f88
yaoyu bao <yaobao5354@gmail.com>
2019-08-26 19:52:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,17 +6,15 @@ def key_for_min_value(name_hash)
   if name_hash == []
     return nil
   else
-    item = []
-    value = []
+    min_item = []
+    min = []
     name_hash.collect do |item, value|
-      min = value
-      if min < value 
-        new_min = value
-        new_item = item
-      else 
-        new_min = min
+      min << value
+      if min[0] > value 
+        min[0] = value
+        min_item << item
       end
-      return new_item
     end
+    return min_item.pop
   end   
 end
