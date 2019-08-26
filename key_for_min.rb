a23fc0d223e59164b66203ae593d3150125d017c
yaoyu bao <yaobao5354@gmail.com>
2019-08-26 19:42:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+  if name_hash == []
+    return nil
+  else
+    name_hash.collect do |item, value|
+      min = value
+      if min < value 
+        new_min = value
+        new_item = item
+      else 
+        new_min = min
+      end
+      return new_item
+    end
+  end   
 end
